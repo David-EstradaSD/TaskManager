1304e50936a1ff0efad33da8b24dc8f185180da6
David <destrada2193@gmail.com>
2021-09-22 14:12:47 -0700

    Create custom hook called use-http.js

---
+++ src/hooks/use-http.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const useHttp = (requestConfig, applyDataFunc) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyDataFunc(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendRequest: sendRequest,
+  }; // recall that we can return ANYTHING from our custom hooks (arrays, strings, objects) so we return the state of this component / custom hook as properties of an object 
+};
+
+export default useHttp;
